bebd979c3cbc2ce848336fd7aed785e9533221f8
dtekluva <kboysreel@gmail.com>
2021-07-14 05:11:44 +0100

    update

--- app.py
+++ app.py
@@ -82,4 +82,6 @@ def static_files(filename):
     return send_from_directory(app.config['STATIC_PATH'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
+
+    
